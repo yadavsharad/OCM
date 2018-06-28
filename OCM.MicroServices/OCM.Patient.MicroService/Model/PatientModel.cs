d977c6cfe4df5a66870282b2aa14cbda53d8c97c
yadavsharad <yadav_sharad@hotmail.com>
2018-06-29 02:03:00 +0530

    Added microservices for patient and doctor

---
+++ OCM.MicroServices/OCM.Patient.MicroService/Model/PatientModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OCM.Patient.MicroService.Model
+{
+    public class PatientModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+
+
+    }
+}
